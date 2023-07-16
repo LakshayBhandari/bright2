baaddd434c17a3bbc141f1df95f2f55efbe09b81
SacCrypt <sachinfulsunge1@gmail.com>
2023-07-16 20:38:27 +0530

    Revert "Removed all copies booked"
    This reverts commit 24912d92f353c7d87a81188d3159b2b5055c9ba0.

--- src/components/BookSlider/index.js
+++ src/components/BookSlider/index.js
@@ -1,136 +1,114 @@
-import { useState, useEffect, useRef } from "react";
-import ScrollContainer from "react-indiana-drag-scroll";
-import "./styles.scss";
-import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
-import "react-loading-skeleton/dist/skeleton.css";
-import { getOverlay } from "./utils";
-import { useSelector, useDispatch } from "react-redux";
-import { getDate } from "../../utils";
-import moment from "moment";
+import {useState, useEffect, useRef} from 'react';
+import ScrollContainer from 'react-indiana-drag-scroll'
+import './styles.scss';
+import {AiOutlineLeft, AiOutlineRight} from 'react-icons/ai';
+import 'react-loading-skeleton/dist/skeleton.css'
+import { getOverlay } from './utils';
+import { useSelector, useDispatch } from 'react-redux';
+import {getDate} from '../../utils';
+import moment from 'moment';
 
 const BookSlider = ({
-  title,
-  books,
-  booksTitle,
-  overlay,
-  showOverlay = true,
+	title,
+	books,
+	booksTitle,
+	overlay,
+	showOverlay = true
 }) => {
-  const {
-    main: { user, isLoggedIn },
-    wishlist: { bucket },
-    book: { age },
-  } = useSelector((state) => state);
-  const dispatch = useDispatch();
-  const [sectionBooks, setSectionBooks] = useState(
-    books.length ? books : ["No books to show"]
-  );
-  const [booksScroll, setBooksScroll] = useState(0);
-  const booksRef = useRef(null);
+	const {main: {user, isLoggedIn}, wishlist: {bucket}, book: {age}} = useSelector(state => state);
+	const dispatch = useDispatch();
+	const [sectionBooks, setSectionBooks] = useState(books.length ? books : ['No books to show']);
+	const [booksScroll, setBooksScroll] = useState(0);
+	const booksRef = useRef(null);
 
-  const slide = (direction, ref) => {
-    const element = ref.current.container.current;
-    setBooksScroll(element.scrollLeft);
-    if (direction === "right")
-      element.scrollTo(element.scrollLeft + element.clientWidth - 100, 0);
-    else element.scrollTo(element.scrollLeft - element.clientWidth - 100, 0);
-  };
+	const slide = (direction, ref) => {
+		const element = ref.current.container.current;
+		setBooksScroll(element.scrollLeft)
+		if(direction === 'right')
+			element.scrollTo(element.scrollLeft + element.clientWidth - 100, 0);
+		else
+			element.scrollTo(element.scrollLeft - element.clientWidth - 100, 0);
+	};
 
-  const getWishlistDeliveryDate = (i, date) => {
-    if (bucket.length)
-      return getDate(
-        moment(date).add(7 * Math.round((i + 1) / user.books_per_week), "days")
-      );
-    return getDate(
-      moment(date).add(
-        7 * (Math.round((i + 1) / user.books_per_week) - 1),
-        "days"
-      )
-    );
-  };
+	const getWishlistDeliveryDate = (i, date) => {
+		if(bucket.length) 
+			return getDate(moment(date).add(7 * (Math.round((i + 1) / user.books_per_week)), 'days'));
+		return getDate(moment(date).add(7 * (Math.round((i + 1) / user.books_per_week) - 1), 'days'));
+	};
 
-  useEffect(() => {
-    const element = booksRef.current.container.current;
-    element.scrollTo(0, 0);
-  }, [age]);
+	useEffect(() => {
+		const element = booksRef.current.container.current;
+		element.scrollTo(0, 0);
+	}, [age]);
 
-  useEffect(() => {
-    if (books.length) setSectionBooks(books);
-    else setSectionBooks(["No books to show"]);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [books]);
+	useEffect(() => {
+		if(books.length)
+			setSectionBooks(books);
+		else
+			setSectionBooks(['No books to show']);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [books]);
 
-  return (
-    <div className="book-slider">
-      {title && <h3 className="title">{title}</h3>}
-      <div className="books">
-        {sectionBooks?.length &&
-          sectionBooks[0] !== "No books to show" &&
-          booksScroll !== 0 && (
-            <AiOutlineLeft
-              className="left-arrow"
-              onClick={() => slide("left", booksRef)}
-            />
-          )}
-        {sectionBooks?.length &&
-          sectionBooks[0] !== "No books to show" &&
-          booksRef?.current?.container.current.scrollWidth >
-            booksRef?.current?.container.current.clientWidth && (
-            <AiOutlineRight
-              className="right-arrow"
-              onClick={() => slide("right", booksRef, true)}
-            />
-          )}
-        {booksTitle && <h3>{booksTitle}</h3>}
-        <ScrollContainer vertical={false} ref={booksRef}>
-          {!sectionBooks?.length || sectionBooks[0] === "No books to show" ? (
-            <h3
-              style={{ textAlign: "center", fontSize: "0.9rem" }}
-              className="no-books-text"
-            >
-              No books to show
-            </h3>
-          ) : (
-            <div className="book-list">
-              {sectionBooks?.map((book, i) => {
-                return (
-                  <div className={`book`} key={i}>
-                    <div className="book-image">
-                      <img src={book.image} alt="Book" />
-                    </div>
-                    <p>{book.name.split(":")[0]}</p>
-                    <div className="book-details">
-                      {book.rating && (
-                        <div className="book-detail">
-                          <p>{book.rating}</p>
-                          <img src="/icons/star.png" alt="Rating" />
-                        </div>
-                      )}
-                      {book.review_count && !isNaN(book.review_count) && (
-                        <div className="book-detail">
-                          <img src="/icons/reviews.png" alt="Reviews" />
-                          <p>{Number(book.review_count).toLocaleString()}</p>
-                        </div>
-                      )}
-                    </div>
-
-                    {showOverlay &&
-                      getOverlay(
-                        overlay,
-                        sectionBooks,
-                        book,
-                        i,
-                        dispatch,
-                        isLoggedIn
-                      )}
-                  </div>
-                );
-              })}
-            </div>
-          )}
-        </ScrollContainer>
-      </div>
-    </div>
-  );
+	return (
+		<div className="book-slider">
+			{title && <h3 className='title'>{title}</h3>}
+			<div className="books">
+				{sectionBooks?.length && sectionBooks[0] !== 'No books to show' && booksScroll !== 0 &&
+					<AiOutlineLeft
+						className='left-arrow'
+						onClick={() => slide('left', booksRef)}
+					/>
+				}
+				{(sectionBooks?.length && sectionBooks[0] !== 'No books to show') && booksRef?.current?.container.current.scrollWidth > booksRef?.current?.container.current.clientWidth &&
+					<AiOutlineRight
+						className='right-arrow'
+						onClick={() => slide('right', booksRef, true)}
+					/>
+				}
+				{booksTitle && <h3>{booksTitle}</h3>}
+				<ScrollContainer vertical={false} ref={booksRef}>
+					{(!sectionBooks?.length || sectionBooks[0] === 'No books to show')
+						?
+						<h3 style={{textAlign: 'center', fontSize: '0.9rem'}} className='no-books-text'>No books to show</h3>
+						:
+						<div className="book-list">
+							{sectionBooks?.map((book, i) => {
+								return (
+									<div 
+										className={`book ${isLoggedIn && !book.stock_available ? 'book-not-available' : ''}`} 
+										key={i}
+									>
+										<div className='book-image'>
+											<img src={book.image} alt='Book'/>
+										</div>
+										<p>{book.name.split(':')[0]}</p>
+										<div className='book-details'>
+											{book.rating && 
+											<div className='book-detail'>
+												<p>{book.rating}</p>
+												<img src='/icons/star.png' alt='Rating'/>
+											</div>}
+											{book.review_count && !isNaN(book.review_count) && 
+											<div className='book-detail'>
+												<img src='/icons/reviews.png' alt='Reviews'/>
+												<p>{Number(book.review_count).toLocaleString()}</p>
+											</div>}
+										</div>
+										{isLoggedIn && (overlay === 'wishlist' || !overlay) && !book.stock_available &&
+											<p className='all-copies-booked'>
+												All Copies Booked
+											</p>										
+										}
+										{showOverlay && getOverlay(overlay, sectionBooks, book, i, dispatch, isLoggedIn)}
+									</div>
+								);
+							})}
+						</div>
+					}
+				</ScrollContainer>
+			</div>
+		</div>
+	);
 };
 
 export default BookSlider;
